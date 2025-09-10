ffe3f28f6e2b932c29bee441043b83da7b565da0
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-09-10 08:28:01 +0700

    Welcome sandevistan

---
+++ Modules/Scripts/SandevBoot.sh
@@ -0,0 +1,23 @@
+change_cpu_gov() {
+  chmod 644 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+  echo "$1" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor >/dev/null
+  chmod 444 /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+}
+
+change_cpu_gov performance
+
+sleep 25
+
+RACO_CONFIG="/data/adb/modules/ProjectRaco/raco.txt"
+
+DEFAULT_CPU_GOV=$(grep '^GOV=' "$RACO_CONFIG" | cut -d'=' -f2)
+
+if [ -z "$DEFAULT_CPU_GOV" ]; then
+    if [ -e /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors ] && grep -q "schedhorizon" /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors; then
+        DEFAULT_CPU_GOV="schedhorizon"
+    else
+        DEFAULT_CPU_GOV="schedutil"
+    fi
+fi
+
+change_cpu_gov "$DEFAULT_CPU_GOV"
