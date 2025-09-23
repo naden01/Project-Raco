e30038cd59590fb7af1b82bfc1b3043967a1ff08
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-09-23 15:08:32 +0700

    Add anya (prep for anya flowstate)

---
+++ Modules/Scripts/AnyaKawaii.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+# Function to find all relevant thermal and logging properties
+get_properties() {
+    getprop | grep 'thermal' | cut -d '[' -f2 | cut -d ']' -f1 | grep -v 'hal'
+}
+
+get_properties | while read -r prop; do
+    if [[ -n "$prop" && "$prop" == init.svc.* ]]; then
+        service=${prop:9}
+        setprop ctl.start "$service"
+    fi
+done
+
+get_properties | while read -r prop; do
+    if [[ -n "$prop" && "$prop" == init.svc.* ]]; then
+        service=${prop:9}
+        start "$service"
+    fi
+done
