8a9fbf0eb5006824d227ea430398b92c478d9358
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-09-26 08:34:12 +0700

    Improve Cache Cleaner by: @HoyoSlave & @Kzuyoo

--- Modules/Scripts/Clear_cache.sh
+++ Modules/Scripts/Clear_cache.sh
@@ -10,9 +10,31 @@ for DIR in /data/data/*; do
   fi
 done
 
-# Cache cleaner by Taka
-find /data/data/*/cache/* -delete 
-find /data/data/*/code_cache/* -delete 
-find /data/user_de/*/*/cache/* -delete 
-find /data/user_de/*/*/code_cache/* -delete 
-find /sdcard/Android/data/*/cache/* -delete 
+# Cache cleaner by Taka, @Kzuyoo, @HoyoSlave
+find /data/data/*/cache/* -delete &>/dev/null
+find /data/data/*/code_cache/* -delete &>/dev/null
+find /data/user_de/*/*/cache/* -delete &>/dev/null
+find /data/user_de/*/*/code_cache/* -delete &>/dev/null
+find /sdcard/Android/data/*/cache/* -delete &>/dev/null
+pm trim-caches 1024G
+cmd stats clear-puller-cache
+cmd activity clear-debug-app
+cmd activity clear-watch-heap -a
+cmd activity clear-exit-info
+cmd content reset-today-stats
+cmd companiondevice refresh-cache
+cmd companiondevice remove-inactive-associations
+cmd blob_store clear-all-blobs
+cmd blob_store clear-all-sessions
+cmd device_policy clear-freeze-period-record
+wm tracing size 0
+cmd font clear
+cmd location_time_zone_manager clear_recorded_provider_states
+cmd lock_settings remove-cache
+cmd media.camera clear-stream-use-case-override
+cmd media.camera watch clear
+cmd safety_center clear-data
+cmd time_detector clear_network_time
+cmd time_detector clear_system_clock_network_time
+dumpsys procstats --clear
+cmd package art cleanup
