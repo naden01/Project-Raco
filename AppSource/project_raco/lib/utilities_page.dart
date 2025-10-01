2fb38d37d61481ec1e4e875fab5f161169ecedb7
LoggingNewMemory <albert.wesley.dion@gmail.com>
2025-10-01 23:00:26 +0700

    f

--- AppSource/project_raco/lib/utilities_page.dart
+++ AppSource/project_raco/lib/utilities_page.dart
@@ -11,7 +11,7 @@ import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:process_run/process_run.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../main.dart'; // Import to access the global themeNotifier
+import '../main.dart';
 import '/l10n/app_localizations.dart';
 
 //region Helper Functions
